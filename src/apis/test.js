d25d4ba20765d215ad2c0d845d251ddd4fd8d8cb
CJY <cjy3458@naver.com>
2025-02-10 17:33:53 +0900

    feat: 구글밋 생성 api 테스트

--- src/apis/test.js
+++ src/apis/test.js
@@ -7,3 +7,27 @@ export const getData = async () => {
 	console.log(result.data);
 	return result.data;
 };
+
+const createMeet = async (accessToken) => {
+	try {
+		const response = await axios.post(
+			'https://meet.googleapis.com/v2/spaces',
+			{},
+			{
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+					'Content-Type': 'application/json',
+				},
+			},
+		);
+
+		const data = response.data;
+		if (data.name) {
+			setMeetLink(`https://meet.google.com/${data.name.split('/')[1]}`);
+		} else {
+			console.error('Google Meet 생성 실패', data);
+		}
+	} catch (error) {
+		console.error('API 요청 오류', error);
+	}
+};
